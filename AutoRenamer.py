7042e3c3976c7998a38f8492e20a8240c249f930
Ethan <10841219@my.uvu.edu>
2022-12-15 21:41:36 -0700

    auto renamer

--- AutoRenamer.py
+++ AutoRenamer.py
@@ -2,8 +2,23 @@ import maya.cmds as cmds
 
 def AutoRenamer(name):
     selection = cmds.ls(sl=True)
+    occurance = name.count("#")
+    split = name.split("_")
+    instance = 1
     for index in range(len(selection)):
-        newName = name + "_" + str(index)
+        num = "0"*(occurance-len(str(instance))) + str(instance)
+        newName = split[0] + "_" + num + "_" + split[2]
         cmds.rename(selection[index], newName)
+        instance += 1
+def GenerateWindow():
+    NamingWindow = "Naming_Window"
+    if cmds.window(NamingWindow, ex = True):
+        cmds.deleteUI(NamingWindow, window = True)
+    NamingWindow = cmds.window("Naming_Window", wh = (500, 300), t = "Naming Window", s = True)
+    ColumnLayout = cmds.columnLayout("Column_Layout", adj = True, p = NamingWindow)
+    NameField = cmds.textField("Text_Field", p = ColumnLayout, pht = "enter in format \"Name_##_NodeType\"")
+    cmds.button("ReNameButton", l = "Rename", p = ColumnLayout, c = 'AutoRenamer(cmds.textField("Text_Field", query = True, tx = True))')
 
-AutoRenamer("lmao")
+    cmds.showWindow(NamingWindow)
+
+GenerateWindow()
